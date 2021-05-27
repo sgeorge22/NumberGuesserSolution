01f59d7fdcef8e65c9f47dbc440a469eb07b8639
Sarah George <sgeorge@tql.com>
2021-05-27 12:03:47 -0400

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        //Code along from a teaching video on YT, Create A Number Guessing Game in C#
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int winNum = r.Next(0, 100);
+            bool win = false;
+            do
+            {
+                Console.WriteLine("Guess a number in between 0 and 100.");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if(i > winNum)
+                {
+                    Console.WriteLine("Too high! Guess lower!");
+                }
+                else if(i < winNum)
+                {
+                    Console.WriteLine("Too low! Guess higher!");
+                }
+                else if(i == winNum)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+            }
+            while (win == false);
+
+
+        }
+    }
+}
